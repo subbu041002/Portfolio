704faf0210457a884c53e635d96b25be2ae40e33
Subbu <subrahmanyakumar@digiryte.com>
2024-04-03 01:04:20 +0530

    added chat

--- projects/script.js
+++ projects/script.js
@@ -110,7 +110,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+    s1.src = 'https://embed.tawk.to/660c54d91ec1082f04de09fd/1hqg2mte1';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
