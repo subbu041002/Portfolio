bf57fc19824cd19f8a9a6f0daeec91e87d96ac30
Subbu <subrahmanyakumar@digiryte.com>
2024-04-03 11:40:03 +0530

    javascript changes

--- projects/script.js
+++ projects/script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
+            document.title = "Projects | Portfolio Subrahmanya Kumar";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
